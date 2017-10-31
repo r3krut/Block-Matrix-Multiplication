8ce8838e3de9f093aad086b4dca82af0f34bbb28
Tananakin Mihail <tananakin.mihail@yandex.ru>
2017-10-31 21:20:57 +0300

    Some fixes

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,7 +5,12 @@ double dRand()
     return ((double)(rand() % 20000 - 10001) / 100.0);
 }
 
-double** create_matrix(const size_t m_size, const bool _type)
+float fRand()
+{
+    return ((float)(rand() % 20000 - 10001) / 100.0);
+}
+
+double** create_double_matrix(const size_t m_size, const bool _type)
 {
     assert(m_size >= 2);
 
@@ -38,16 +43,37 @@ double** create_matrix(const size_t m_size, const bool _type)
     return mat;
 }
 
-void write_to_file(const double *lin_mat, const size_t elems_count, const std::string &file_name)
+float** create_float_matrix(const size_t m_size, const bool _type)
 {
-    assert(lin_mat != NULL && elems_count > 0);
+    assert(m_size >= 2);
 
-    std::ofstream of(file_name);
-    if (!of.is_open())
-        throw std::runtime_error("File '" + file_name + "' dot't was created.\n");
-    for (size_t i = 0; i < elems_count; i++)
-        of << lin_mat[i] << " ";
-    of.close();
+    float **mat = new float*[m_size];
+    for (size_t i = 0; i < m_size; i++)
+        mat[i] = new float[m_size];
+
+    if (_type)
+    {
+        //top-triangular
+        for (size_t i = 0; i < m_size; i++)
+            for (size_t j = 0; j < m_size; j++)
+            {
+                if (i > j)
+                    mat[i][j] = 0.0;
+                else
+                    mat[i][j] = fRand();
+            }
+    }
+    else
+    {
+        //symmetric
+        for (size_t i = 0; i < m_size; i++)
+            for (size_t j = i; j < m_size; j++)
+            {
+                mat[i][j] = fRand();
+                mat[j][i] = mat[i][j];
+            }
+    }
+    return mat;
 }
 
 void create_csv_file(std::list<std::pair<size_t, double> > &list, const std::string &file_name)
